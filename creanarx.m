f97f176877708247634add0ad9fcf4c94b9defda
Ricardo Cavieses <rcavieses@gmail.com>
2017-05-22 10:50:36 -0600

    Update creanarx.m

--- creanarx.m
+++ creanarx.m
@@ -1,8 +1,6 @@
-function [netc]=creanarx(InputTrain, TargTrain,ID,HLS)
-%[netc]=creanarx(InputTrain, TargTrain,ID,HLS)
-inputSeries =tonndata(InputTrain,0,0);
-targetSeries = tonndata(TargTrain,0,0);
-
+function [netc,net]=creanarx(InputTrain, TargTrain,ID,HLS)
+inputSeries = InputTrain;
+targetSeries = TargTrain;
 inputDelays = 1:ID;
 feedbackDelays = 1:ID;
 hiddenLayerSize = HLS;
@@ -13,7 +11,7 @@ net.divideParam.trainRatio = 90/100;
 net.divideParam.valRatio = 5/100;
 net.divideParam.testRatio = 5/100;
 
-[net,tr] = train(net,inputs,targets,inputStates,layerStates);
+net = train(net,inputs,targets,inputStates,layerStates);
 
 netc = closeloop(net);
 netc.name = [net.name ' - Closed Loop'];
